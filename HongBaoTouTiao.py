824649f456f8540b67f50290900b0c177dec2ddc
liuqikang <liuqikang@jd.com>
2019-03-03 01:11:18 +0800

    红包头条阅读

--- HongBaoTouTiao.py
+++ HongBaoTouTiao.py
@@ -34,128 +34,71 @@ downTicks = 3
 currentClick = 0
 # 检索图
 imgSearch = cv2.imread("hongbaotoutiao/search_ad.jpg", 0)
-# imgSearch4 = cv2.imread("search4.jpg", 0)
-# imgSearch5 = cv2.imread("search5.jpg", 0)
-# imgSearch6 = cv2.imread("search6.jpg", 0)
-#
-#
-# def findsearch():
-#     exeCount = 0
-#     # 查找 检索图 位置
-#     while True:
-#         if exeCount > 10:
-#             print("搜索不到")
-#             exit()
-#         # 截图
-#         os.system("adb shell screencap -p /sdcard/screen.jpg")
-#         # 推送 到当前目录下
-#         os.system("adb pull /sdcard/screen.jpg %s" % (os.path.abspath('.')))
-#         # 读取灰度图
-#         imgGray = cv2.imread("screen.jpg", 0)
-#         # 匹配模板 广告 imgSearch 32
-#         rightx = 32
-#         res = cv2.matchTemplate(imgGray, imgSearch, cv2.TM_SQDIFF_NORMED)
-#         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-#         if (min_loc[0] > (rightx - 5)) & (min_loc[0] < (rightx + 5)):
-#             # 在屏幕内
-#             return min_loc[0], min_loc[1]
-#         # 不在,向上滑动2/3屏幕，重新检索
-#         os.system("adb shell input swipe 600 1700 600 840 500")  # 按住滑动
-# 
-# 
-# # 主程序逻辑
-# while True:
-#     # 找广告坐标
-#     x, y = findsearch()
-#     # 没找到广告坐标 说明本页面
-#     while True:
-#         # 在屏幕下半部分，滚动到屏幕上半部分
-#         if y > 1500:
-#             os.system("adb shell input swipe 600 1700 600 700 500")  # 向上滑动
-#         elif y > 1000:
-#             os.system("adb shell input swipe 600 1200 600 800 500")  # 向上滑动
-#         elif y > 500:
-#             os.system("adb shell input swipe 600 1000 600 800 500")  # 向上滑动
-#         else:
-#             break
-#         # 再获取一遍位置
-#         x, y = findsearch()
-#     # 点进广告下方第一篇文章
-#     os.system("adb shell input tap 600 %d" % (y + 300))
-#     # 浏览25秒
-#     # 记录开始时间
-#     start = time.time()
-#     # 每间隔1s循环向下/向上翻页，持续固定时长X
-#     while (time.time() - start) < seeTime:
-#         x = random.randint(0, 100)
-#         os.system("adb shell input swipe 600 600 600 %d 400" % (500 - x))  # 慢慢向上滑动
-#         time.sleep(0.5)
-#     # 退出
-#     os.system("adb shell input keyevent 4")
-#     # 点进广告下方第二篇文章
-#     os.system("adb shell input tap 600 %d" % (y + 800))
-#     # 浏览25秒
-#     # 记录开始时间
-#     start = time.time()
-#     # 每间隔1s循环向下/向上翻页，持续固定时长X
-#     while (time.time() - start) < seeTime:
-#         x = random.randint(0, 100)
-#         os.system("adb shell input swipe 600 600 600 %d 400" % (500 - x))  # 慢慢向上滑动
-#         time.sleep(0.5)
-#     # 退出
-#     os.system("adb shell input keyevent 4")
-#     # 点进广告下方第三篇文章
-#     os.system("adb shell input tap 600 %d" % (y + 1200))
-#     # 浏览25秒
-#     # 记录开始时间
-#     start = time.time()
-#     # 每间隔1s循环向下/向上翻页，持续固定时长X
-#     while (time.time() - start) < seeTime:
-#         x = random.randint(0, 100)
-#         os.system("adb shell input swipe 600 600 600 %d 400" % (500 - x))  # 慢慢向上滑动
-#         time.sleep(0.5)
-#     # 退出
-#     os.system("adb shell input keyevent 4")
-#     # 向下滚动
-#     os.system("adb shell input swipe 600 1200 750 %d 400" % (500 - x))  # 按住滑动
+imgSearch2 = cv2.imread("hongbaotoutiao/search_ad2.jpg", 0)
 
-    # # 记录开始时间
-    # start = time.time()
-    # # 每间隔1s循环向下/向上翻页，持续固定时长X
-    # while (current - start) < seeTime:
-    #     x = random.randint(0, 100)
-    #     os.system("adb shell input swipe 600 600 600 %d 400" % (500 - x))  # 按住滑动
-    #     current = time.time()
-    #     time.sleep(1)
-    # # 快速向下滑动几秒 直到底
-    # i = 0
-    # while downTicks != i:
-    #     i += 1
-    #     os.system("adb shell input swipe 600 2000 600 0 100")  # 按住滑动
-    # # 查找检索图位置
-    # x, y = findsearch()
-    # # 打印一下
-    # print("检索到目标位置: %d   %d" % (x, y))
-    # # 如果在屏幕下半部分 向上滑动500 慢慢的
-    # if y > 1000:
-    #     os.system("adb shell input swipe 600 1300 600 800 1000")  # 按住滑动
-    #     # 再获取一遍位置
-    #     x, y = findsearch()
-    #     # 打印一下
-    #     print("检索到目标位置: %d   %d" % (x, y))
-    #
-    # # 这块不完善
-    # # 假设没有评论的话，如果没拖动说检索错误了,被最底下的广告影响了
-    # if y > 1800:
-    #     # 向上盲点一下，先暂时这样
-    #     # 点击 下一个文章的相对位置 文章item高度280
-    #     os.system("adb shell input tap 600 %d" % (y - 1200))
-    # else:
-    #     # 高度自身偏移85 计算广告item底边高度
-    #     y += 85
-    #     # 点击 下一个文章的相对位置 文章item高度280
-    #     os.system("adb shell input tap 600 %d" % (y + 280 / 2))
+#
+def findsearch():
+    exeCount = 0
+    # 查找 检索图 位置
+    while True:
+        if exeCount > 10:
+            print("搜索不到")
+            exit()
+        # 截图
+        os.system("adb shell screencap -p /sdcard/screen.jpg")
+        # 推送 到当前目录下
+        os.system("adb pull /sdcard/screen.jpg %s" % (os.path.abspath('.')))
+        # 读取灰度图
+        imgGray = cv2.imread("screen.jpg", 0)
+        # 匹配模板 广告 imgSearch 32
+        rightx = 32
+        res = cv2.matchTemplate(imgGray, imgSearch, cv2.TM_SQDIFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if (min_loc[0] > (rightx - 5)) & (min_loc[0] < (rightx + 5)):
+            # 在屏幕内
+            return min_loc[0], min_loc[1]
+        # 匹配模板 广告 imgSearch2 64
+        rightx = 64
+        res = cv2.matchTemplate(imgGray, imgSearch2, cv2.TM_SQDIFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if (min_loc[0] > (rightx - 5)) & (min_loc[0] < (rightx + 5)):
+            # 在屏幕内
+            return min_loc[0], min_loc[1]
+        # 不在,向上滑动2/3屏幕，重新检索
+        os.system("adb shell input swipe 600 1700 600 840 500")  # 按住滑动
 
+
+# 主程序逻辑
+while True:
+    # 找广告坐标
+    x, y = findsearch()
+    # 没找到广告坐标 说明本页面
+    while True:
+        # 在屏幕下半部分，滚动到屏幕上半部分
+        if y > 1500:
+            os.system("adb shell input swipe 600 1700 600 700 500")  # 向上滑动
+        elif y > 1000:
+            os.system("adb shell input swipe 600 1200 600 800 500")  # 向上滑动
+        elif y > 500:
+            os.system("adb shell input swipe 600 1000 600 800 500")  # 向上滑动
+        else:
+            break
+        # 再获取一遍位置
+        x, y = findsearch()
+    # 点进广告下方第一篇文章
+    os.system("adb shell input tap 600 %d" % (y + 300))
+    # 浏览25秒
+    # 记录开始时间
+    start = time.time()
+    # 每间隔1s循环向下/向上翻页，持续固定时长X
+    while (time.time() - start) < seeTime:
+        x = random.randint(0, 100)
+        os.system("adb shell input swipe 600 1600 600 %d 400" % (1500 - x))  # 慢慢向上滑动
+        time.sleep(0.5)
+    # 退出
+    os.system("adb shell input tap 87 157 ")
+    # 向下滚动
+    os.system("adb shell input swipe 600 1200 750 %d 400" % (500 - x))  # 按住滑动
 
 # class ARQuTouTiao(object):
 #     # 下滑次数
--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from QuTouTiao import ARQuTouTiao
-from JuKanDian import ARJuKanDian
-
-# 时间单位 1小时
-appTimeBase = 3600
-# 单篇阅读时长 30秒
-readTimeBase = 30
-
-"""
-app名称
-App阅读时长
-文章阅读时长
-"""
-# 趣头条
-appTime_qtt = appTimeBase * 1
-readTime_qtt = readTimeBase * 2
-
+# from QuTouTiao import ARQuTouTiao
+# from JuKanDian import ARJuKanDian
+#
+# # 时间单位 1小时
+# appTimeBase = 3600
+# # 单篇阅读时长 30秒
+# readTimeBase = 30
+#
+# """
+# app名称
+# App阅读时长
+# 文章阅读时长
+# """
+# # 趣头条
+# appTime_qtt = appTimeBase * 2
+# readTime_qtt = readTimeBase * 2
+#
 # autoReadQuTouTiao = ARQuTouTiao(appTime_qtt, readTime_qtt)
 # autoReadQuTouTiao.read()
-
-# 聚看点
-appTime_qtt = appTimeBase * 2
-readTime_qtt = 40 * 3
-
-autoJuKanDian = ARJuKanDian(appTime_qtt, readTime_qtt)
-autoJuKanDian.read()
+#
+# # 聚看点
+# appTime_qtt = appTimeBase * 3
+# readTime_qtt = 40 * 3
+#
+# # autoJuKanDian = ARJuKanDian(appTime_qtt, readTime_qtt)
+# # autoJuKanDian.read()

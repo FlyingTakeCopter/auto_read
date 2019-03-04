58aba624d0e4b93e5104e893cad64fc287ef8437
liuqikang <liuqikang@jd.com>
2019-03-04 20:25:17 +0800

    提交

--- ShuaBao.py
+++ ShuaBao.py
@@ -8,7 +8,7 @@ class ARShuaBao(object):
         self.apptime = apptime
         self.readtime = readtime
 
-    def read(self):
+    def read(self, devices):
         appstart = time.time()
         # 主程序逻辑 执行时长小于总时长
         while (time.time() - appstart) < self.apptime:
@@ -18,5 +18,7 @@ class ARShuaBao(object):
             # 每间隔1s循环向下/向上翻页，持续固定时长X
             while (time.time() - start) < self.readtime:
                 time.sleep(10)
-            os.system("adb shell input swipe 600 600 600 200 200")  # 慢慢向上滑动
+            # 设备循环执行
+            for dName in devices:
+                os.system("adb -s " + dName + " shell input swipe 300 800 300 200 100")  # 后台执行 小米800-200
         print("阅读完成：刷宝")
--- main.py
+++ main.py
@@ -2,6 +2,28 @@ from QuTouTiao import ARQuTouTiao
 from JuKanDian import ARJuKanDian
 from HongBaoTouTiao import ARHongBaoTouTiao
 from ShuaBao import ARShuaBao
+import os
+
+# 获取模拟器总数
+def getDevicesAll():
+    devices = []
+    try:
+        for dName_ in os.popen("adb devices"):
+            if "\t" in dName_:
+                # 真机
+                devices.append(dName_.split("\t")[0])
+                # 模拟器检索
+                # findres = dName_.find("emulator")
+                # if findres != -1:
+                #     devices.append(dName_.split("\t")[0])
+        devices.sort(cmp=None, key=None, reverse=False)
+    except:
+        pass
+    print("\n设备名称: %s \n总数量:%s台" % (devices, len(devices)))
+    return devices
+
+
+devices = getDevicesAll()
 
 # 时间单位 1小时
 appTimeBase = 3600
@@ -36,7 +58,7 @@ App阅读时长
 
 # 刷宝
 appTime_qtt = appTimeBase * 3
-readTime_qtt = 30
+readTime_qtt = 10
 
 autoShuaBao = ARShuaBao(appTime_qtt, readTime_qtt)
-autoShuaBao.read()
+autoShuaBao.read(devices)
